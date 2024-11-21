64621e4dcb55708ad288382eef16cb0bfab243d4
pacasiano <peterandre.casiano@icloud.com>
2024-11-21 21:59:37 +0800

    update2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/web-homepage/', // Add this line with the name of your repository
 });
